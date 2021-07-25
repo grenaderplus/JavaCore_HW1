f64365b5c56a564d97baf66448cc18717e3ee680
Alexander <grenaderplus@gmail.com>
2021-07-25 13:33:57 +0300

    HW_10

--- src/main/java/HW_10/PhoneList.java
+++ src/main/java/HW_10/PhoneList.java
@@ -1,4 +1,20 @@
 package HW_10;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class PhoneList {
+
+    private HashMap<String, Set<String>> phonelist = new HashMap<>();
+
+    public void add(String name, String phone) {
+        Set<String> number = phonelist.getOrDefault(name, new HashSet<>());
+        number.add(phone);
+        phonelist.put(name, number);
+    }
+
+    public Set<String> get(String name) {
+        return phonelist.get(name);
+    }
 }
